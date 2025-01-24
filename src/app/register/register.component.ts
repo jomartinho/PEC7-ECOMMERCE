45ec9845841561b551e6317ca1e015ac3a8359c0
joao rebordao martinho <jmartinho@uoc.edu>
2025-01-24 17:33:45 +0000

    completar register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-register',
-  imports: [],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.registerForm = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      this.userService.register(this.registerForm.value).subscribe(
+        (result) => {
+          console.log('Registration successful', result);
+          alert('User registered successfully!');
+        },
+        (error) => {
+          console.error('Registration failed', error);
+          alert('Error registering user. Please try again.');
+        }
+      );
+    }
+  }
 }
