fe1ee15f6a449bde30ec73b3b7693b06f590ec28
joao rebordao martinho <jmartinho@uoc.edu>
2025-01-24 17:31:55 +0000

    completar componente login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private userService: UserService) {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.userService.login(this.loginForm.value).subscribe(
+        (result) => console.log('Login successful', result),
+        (error) => console.error('Login failed', error)
+      );
+    }
+  }
 }
