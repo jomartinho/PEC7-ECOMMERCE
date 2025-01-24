740efbf70d8664efe8285fd26d60df5727f583a3
joao rebordao martinho <jmartinho@uoc.edu>
2025-01-24 17:36:07 +0000

    completar componente article-detail

--- src/app/article-detail/article-detail.component.ts
+++ src/app/article-detail/article-detail.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../services/article.service';
 
 @Component({
   selector: 'app-article-detail',
-  imports: [],
   templateUrl: './article-detail.component.html',
-  styleUrl: './article-detail.component.css'
 })
-export class ArticleDetailComponent {
+export class ArticleDetailComponent implements OnInit {
+  article: any;
 
+  constructor(private route: ActivatedRoute, private articleService: ArticleService) {}
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.articleService.getArticleById(id).subscribe((data) => {
+      this.article = data;
+    });
+  }
 }
